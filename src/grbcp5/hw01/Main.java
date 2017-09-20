d8509de67ade9ece01a81817c51ae0b6b82a7bb9
grbcp5 <grbcp5@mst.edu>
2017-09-20 17:12:03 -0500

    Create configFileReader

--- src/grbcp5/hw01/Main.java
+++ src/grbcp5/hw01/Main.java
@@ -1,70 +1,37 @@
 package grbcp5.hw01;
 
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import grbcp5.hw01.input.ConfigFileReader;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 public class Main {
 
   public static void main( String[] args ) {
 
-    /* Local variables */
-    File xmlFile;
-    DocumentBuilderFactory documentBuilderFactory;
-    DocumentBuilder documentBuilder;
-    Document document;
-    Node rootNode;
-    NodeList parameters;
-    Node node;
-    Element element;
+    ConfigFileReader configFileReader;
+    Map< String, Object > parameters;
 
-    /* Reference File */
-    xmlFile = new File( args[ 0 ] );
-    documentBuilderFactory = DocumentBuilderFactory.newInstance();
-
-    /* Try to read file */
     try {
 
-      /* Throws ParserConfigurationException */
-      documentBuilder = documentBuilderFactory.newDocumentBuilder();
+      /* Try to read the file */
+      configFileReader = new ConfigFileReader( args[ 0 ] );
 
-      /* Throws SAXException, IOException */
-      document = documentBuilder.parse( xmlFile );
-
-      /* Read document */
-      document.getDocumentElement().normalize();
-      rootNode = document.getChildNodes().item( 0 );
-      parameters = rootNode.getChildNodes();
-
-      /* For each child of the root node */
-      for( int p = 0; p < parameters.getLength(); p++ ) {
-        node = parameters.item( p );
-
-        if (node.getNodeType() == Node.ELEMENT_NODE) {
-          element = ( Element )( node );
-          System.out.print( element.getTagName() + ": " );
-          System.out.println( element.getTextContent() );
-        }
-
-      }
-
-    /* Catch errors */
-    } catch ( ParserConfigurationException e ) {
+    } catch ( ParserConfigurationException | IOException | SAXException e ) {
       e.printStackTrace();
-    } catch ( SAXException e ) {
-      e.printStackTrace();
-    } catch ( IOException e ) {
-      e.printStackTrace();
+      return;
     }
+
+    /* Get parameters */
+    parameters = configFileReader.getParameters();
+    Double d = ( Double ) ( parameters.get( "testDouble" ) );
+    Integer i = ( Integer )( parameters.get( "testInteger" ) );
+    String s = ( String )( parameters.get( "testString" ) );
+
+    System.out.println( "Double: " + d + "\nInteger: " + i + "\nString: " + s );
 
   } /* Main function */
 
---
+++ src/grbcp5/hw01/input/ConfigFileReader.java
@@ -0,0 +1,97 @@
+package grbcp5.hw01.input;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConfigFileReader {
+
+  private final File xmlFile;
+  private final Document document;
+  private final Node rootNode;
+  private final NodeList parameterNodeList;
+  private final int numChildNodes;
+
+  public ConfigFileReader( final String configFilePath ) throws
+    ParserConfigurationException, IOException, SAXException {
+
+    /* Local variables */
+    DocumentBuilderFactory documentBuilderFactory;
+    DocumentBuilder documentBuilder;
+    NodeList parameters;
+    Node node;
+    Element element;
+
+    xmlFile = new File( configFilePath );
+    documentBuilderFactory = DocumentBuilderFactory.newInstance();
+
+    /* Throws ParserConfigurationException */
+    documentBuilder = documentBuilderFactory.newDocumentBuilder();
+
+    /* Throws SAXException, IOException */
+    this.document = documentBuilder.parse( this.xmlFile );
+
+    /* Read document */
+    this.document.getDocumentElement().normalize();
+    this.rootNode = document.getChildNodes().item( 0 );
+    this.parameterNodeList = rootNode.getChildNodes();
+    this.numChildNodes = this.parameterNodeList.getLength();
+
+
+  }
+
+  public Map< String, Object > getParameters() {
+    Map< String, Object > parameterMap
+      = new HashMap< String, Object >( this.numChildNodes );
+    Node currentNode;
+    Element element;
+
+    /* For each child of the root node */
+    for( int p = 0; p < this.numChildNodes; p++ ) {
+      currentNode = this.parameterNodeList.item( p );
+
+      if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
+        element = ( Element )( currentNode );
+        String nodeText = element.getTextContent();
+        Object value;
+
+        try {
+
+          /* Try to parse as an integer */
+          value = Integer.parseInt( nodeText );
+
+        } catch ( NumberFormatException e ) {
+
+          try {
+
+            /* Try to parse as a double */
+            value = Double.parseDouble( nodeText );
+
+          } catch ( NumberFormatException e1 ) {
+
+            /* Just assume string */
+            value = nodeText;
+
+          } /* Try double */
+
+        } /* Try integer */
+
+        parameterMap.put( element.getTagName(), value );
+      }
+
+    }
+
+    return parameterMap;
+  }
+
+}

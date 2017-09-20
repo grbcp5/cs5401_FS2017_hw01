52ab8339b96ff5b3c30d532392840d5d840566ef
grbcp5 <grbcp5@mst.edu>
2017-09-20 17:48:44 -0500

    Add prompt for if recieved invalid input

--- src/grbcp5/hw01/Main.java
+++ src/grbcp5/hw01/Main.java
@@ -10,8 +10,36 @@ import java.util.Map;
 
 public class Main {
 
+  private static void printDashedln( int length ) {
+    for( int i = 0; i < length; i++ ) {
+      System.out.print( '-' );
+    }
+    System.out.println();
+  }
+
+  private static void printDashedln() {
+    printDashedln( 80 );
+  }
+
+   private static void printInvalidInput() {
+
+    printDashedln();
+    System.out.println(
+      "\nUsage: main <configFile> <problemDefinitionFile>\n"
+    );
+    printDashedln();
+
+  }
+
   public static void main( String[] args ) {
 
+    if( args.length < 2 ) {
+
+      printInvalidInput();
+      return;
+    }
+
+    /* Local Variables */
     ConfigFileReader configFileReader;
     Map< String, Object > parameters;
 
@@ -22,6 +50,7 @@ public class Main {
 
     } catch ( ParserConfigurationException | IOException | SAXException e ) {
       e.printStackTrace();
+      printInvalidInput();
       return;
     }
 

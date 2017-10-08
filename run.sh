cdb43f322c4342ae7a9a38eeb57fa09ec035a4bb
grbcp5 <grbcp5@mst.edu>
2017-10-08 17:51:44 -0500

    Fix type errors

--- run.sh
+++ run.sh
@@ -8,20 +8,20 @@ mkdir sol/50Shapes
 mkdir log/100Shapes
 mkdir sol/100Shapes
 mkdir log/100ShapesComplex
-mkdir log/100ShapesComplex
+mkdir sol/100ShapesComplex
 
 mkdir log/50Shapes/penalty
 mkdir sol/50Shapes/penalty
 mkdir log/50Shapes/selfAdaptive
 mkdir sol/50Shapes/selfAdaptive
 mkdir log/100Shapes/penalty
-mkdir log/100Shapes/penalty
-mkdir sol/100Shapes/selfAdaptive
+mkdir sol/100Shapes/penalty
+mkdir log/100Shapes/selfAdaptive
 mkdir sol/100Shapes/selfAdaptive
 mkdir log/100ShapesComplex/penalty
-mkdir log/100ShapesComplex/penalty
+mkdir sol/100ShapesComplex/penalty
 mkdir log/100ShapesComplex/selfAdaptive
-mkdir log/100ShapesComplex/selfAdaptive
+mkdir sol/100ShapesComplex/selfAdaptive
 
 javac  -verbose $( find ./src/* -not -name "*Test.java" | grep .java ) -d out/
 java -cp out grbcp5.hw01.Main $1 $2

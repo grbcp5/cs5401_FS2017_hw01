1db641d0f544da4eca9e39842d4e69206b2fde5a
grbcp5 <grbcp5@mst.edu>
2017-10-22 19:33:01 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,25 +3,15 @@
 mkdir log
 mkdir sol
 
-mkdir log/50Shapes
-mkdir sol/50Shapes
-mkdir log/100Shapes
-mkdir sol/100Shapes
-mkdir log/100ShapesComplex
-mkdir sol/100ShapesComplex
+mkdir log/1d
+mkdir sol/1d
 
-mkdir log/50Shapes/penalty
-mkdir sol/50Shapes/penalty
-mkdir log/50Shapes/selfAdaptive
-mkdir sol/50Shapes/selfAdaptive
-mkdir log/100Shapes/penalty
-mkdir sol/100Shapes/penalty
-mkdir log/100Shapes/selfAdaptive
-mkdir sol/100Shapes/selfAdaptive
-mkdir log/100ShapesComplex/penalty
-mkdir sol/100ShapesComplex/penalty
-mkdir log/100ShapesComplex/selfAdaptive
-mkdir sol/100ShapesComplex/selfAdaptive
+mkdir log/1d/50Shapes
+mkdir sol/1d/50Shapes
+mkdir log/1d/100Shapes
+mkdir sol/1d/100Shapes
+mkdir log/1d/100ComplexShapes
+mkdir sol/1d/100ComplexShapes
 
 javac  -verbose $( find ./src/* -not -name "*Test.java" | grep .java ) -d out/
 java -cp out grbcp5.hw01.Main $1 $2
